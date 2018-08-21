95874b548b869cbf01fd06288f996b31db4e6f5f
kuntal <kuntal@nettrackers.net>
2018-08-21 18:17:53 +0530

    after solve cross origin

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const config =require('./models/config');
+const cors = require('cors');
 const app = express();
 
 const db = mongoose.connect('mongodb://localhost:27017/meanAuth', (err, response) => {
@@ -17,6 +18,8 @@ const db = mongoose.connect('mongodb://localhost:27017/meanAuth', (err, response
 
 app.set('port', process.env.port ||  3000);
 app.use(bodyparser.json());
+app.use(cors());
+// Add headers
 
 
 app.post('/register', (req, res) => {
@@ -29,27 +32,18 @@ app.post('/register', (req, res) => {
     user.hashPassword = hashPassword;
     user.save((err, result)=>{
         if(err){ 
-            res.status(500).send("There was a problem registering the user.");
+            res.send({success:"There was a problem registering the user!", status: 500});
          }else{ 
             //jwt auth
             var token = jwt.sign({ id: result._id }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
             });
-            res.status(200).send({ auth: true, token: token }); 
+            res.send({ auth: true, token: token , success:"Registration Successfully Completed!", status:200}); 
         }
     }); 
 });
 
-app.get('/me', function(req, res) {
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token, config.secret, function(err, decoded) {
-      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-      
-      res.status(200).send(decoded);
-    });
-});
+
 
 app.listen(app.get('port'),function(){
     console.log('app is runnitng on - '+ app.get('port'));
